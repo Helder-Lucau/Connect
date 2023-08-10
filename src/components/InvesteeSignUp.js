cf2e8eecff8fcce94e65cbabe90c7a06ed74cfbd
FridaGathima <frida.gathima@student.moringaschool.com>
2023-08-10 16:31:43 +0300

    login & investee registration form updated

--- src/components/InvesteeSignUp.js
+++ src/components/InvesteeSignUp.js
@@ -2,8 +2,8 @@ import React from "react"
 import { useState } from "react"
 
 
-function InvesteeSignUp () {
-    
+function InvesteeSignUp() {
+
     const [name, setName] = useState('')
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
@@ -20,59 +20,59 @@ function InvesteeSignUp () {
     const handleBusinessCategory = (e) => setBusinessCategory(e.target.value)
     const handleBusinessDescription = (e) => setBusinessDescription(e.target.value)
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault()
         console.log(name)
     }
-    
+
 
     return (
-        <form className="investeesignupform" onSubmit={handleSubmit} >
-            <h1>Welcome to Connect</h1>
+        <>
+            <form className="investeesignupform" onSubmit={handleSubmit} >
+                <h1>Welcome to Connect</h1>
 
-            <label htmlFor="name"> Full Name</label>
-            <input value={name} onChange={handleName} type="name" placeholder="full name" id="name"/>
+                <label htmlFor="name"> Full Name</label>
+                <input value={name} onChange={handleName} type="name" placeholder="full name" id="name" />
 
-            <label htmlFor="username">Username</label>
-            <input value={username} onChange={handleUsername} type="username" placeholder="username" id="username"/>
+                <label htmlFor="username">Username</label>
+                <input value={username} onChange={handleUsername} type="username" placeholder="username" id="username" />
 
-            <label htmlFor="password">Password</label>
-            <input value={password} onChange={handlePassword} type="password" placeholder="********"id="password"/>
+                <label htmlFor="password">Password</label>
+                <input value={password} onChange={handlePassword} type="password" placeholder="********" id="password" />
 
-            <label htmlFor="about">About</label>
-            <input value={about} onChange={handleAbout} type="text" placeholder="Tell us a bit about yourself!" id="about"/>
+                <label htmlFor="about">About</label>
+                <input value={about} onChange={handleAbout} type="text" placeholder="Tell us a bit about yourself!" id="about" />
 
-            <label htmlFor="businessName">Business Name</label>
-            <input value={businessName} onChange={handleBusinessName} type="text" id="businessName"/>
+                <label htmlFor="businessName">Business Name</label>
+                <input value={businessName} onChange={handleBusinessName} type="text" id="businessName" />
 
-            <label htmlFor="businessCategory">Business Category</label>
-            {/* <input type="text" id="businessCategory"/> */}
-            <select id="businessCategory">
-                <option value={businessCategory} onChange={handleBusinessCategory} >Healthcare</option> 
-                <option value={businessCategory} onChange={handleBusinessCategory}>Real Estate</option>
-                <option value={businessCategory} onChange={handleBusinessCategory}>Financial Services</option>
-                <option value={businessCategory} onChange={handleBusinessCategory}>information Technology (IT)</option> 
-                <option value={businessCategory} onChange={handleBusinessCategory}>Food Services</option>
-            </select>
+                <label htmlFor="businessCategory">Business Category</label>
+                {/* <input type="text" id="businessCategory"/> */}
+                <select id="businessCategory">
+                    <option value={businessCategory} onChange={handleBusinessCategory} >Healthcare</option>
+                    <option value={businessCategory} onChange={handleBusinessCategory}>Real Estate</option>
+                    <option value={businessCategory} onChange={handleBusinessCategory}>Financial Services</option>
+                    <option value={businessCategory} onChange={handleBusinessCategory}>information Technology (IT)</option>
+                    <option value={businessCategory} onChange={handleBusinessCategory}>Food Services</option>
+                </select>
 
-            <label htmlFor="businessDescription">Business Description</label>
-            <input value={businessDescription} onChange={handleBusinessDescription} type="text" placeholder="Tell us a bit about your business/ business idea!" id="businesDescription"/>
+                <label htmlFor="businessDescription">Business Description</label>
+                <input value={businessDescription} onChange={handleBusinessDescription} type="text" placeholder="Tell us a bit about your business/ business idea!" id="businesDescription" />
 
-             <button type="submit" >Sign Up</button>
+                <button type="submit" >Sign Up</button>
 
-            {/* <label htmlFor="type">Account Type</label>
+                {/* <label htmlFor="type">Account Type</label>
             select tag can also work on the business category
             <select id="type">
                 <option value= "investor">Investor</option> 
                 <option value="investee">Investee</option>
             </select> */}
+                {/* <input type="submit" value="Sign Up" /> */}
+            </form>
 
-           
+            <button>Already have an Account? Login here.</button>
 
-            {/* <input type="submit" value="Sign Up" /> */}
-
-        </form>
-
+        </>
     )
 }
 
