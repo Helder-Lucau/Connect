11d5cfe899168bdfdf6841899cb41c6be04f35b4
FridaGathima <frida.gathima@student.moringaschool.com>
2023-08-10 16:42:22 +0300

    removed login from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 // import './App.css';
 // import index from  './css';
 
-import InvesteeSignUp from "./InvesteeSignUp";
-import Login from "./Login";
+// import InvesteeSignUp from "./InvesteeSignUp";
+// import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
-      <InvesteeSignUp />
-      <Login />
+      
     </div>
   );
 }
